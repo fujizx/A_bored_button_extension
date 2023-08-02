5215f6d3e0c12127ea7eada552cdb83c99758354
Fujizx <fujizx@163.com>
2023-08-02 16:32:51 +0800

    取不到tabs的问题

--- A_bored_button/background.js
+++ A_bored_button/background.js
@@ -21,16 +21,23 @@ async function getRandomWeb() {
   const item = getRandomItem(data);
   const url = item.url;
 
-  // 向内容脚本发送消息，请求在当前页面跳转
+  // 向内容脚本发送消息，请求在当前页面跳转，并传递 item.title
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { command: 'navigate', url: url, title: item.title });
+    if (tabs && tabs.length > 0) {
+      chrome.tabs.sendMessage(tabs[0].id, { command: 'navigate', url: url, title: item.title });
+    } else {
+      // 如果没有活动标签页，创建一个新的标签页并打开链接
+      chrome.tabs.create({ url: url }, function (tab) {
+        chrome.tabs.sendMessage(tab.id, { command: 'show_title', title: item.title });
+      });
+    }
   });
 }
 
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.command === 'start') {
-        count = request.count;
-        getRandomWeb();
-    }
+  if (request.command === 'start') {
+    count = request.count;
+    getRandomWeb();
+  }
 });
+
