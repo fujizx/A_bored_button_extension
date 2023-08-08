2550fe9296539843529cc895495bc991aa870792
Fujizx <fujizx@163.com>
2023-08-08 14:24:59 +0800

    try activeTab

--- A_bored_button/background.js
+++ A_bored_button/background.js
@@ -23,27 +23,18 @@ async function getRandomWeb() {
   const item = getRandomItem(data);
   const url = item.url;
 
-  // 获取当前活动的标签页
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var currTab = tabs[0];
-    if (currTab) { // 如果存在一个活动的标签页
-      // 在当前标签页打开新的URL，并等待标签页加载完成
-      chrome.tabs.update(currTab.id, { url: url }, function (tab) {
-        // 在标签页加载完成后，向content.js发送消息
-        chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-          if (tabId === tab.id && changeInfo.status === "complete") {
-            chrome.tabs.onUpdated.removeListener(listener);
-            chrome.tabs.sendMessage(tabId, { command: 'show_title', title: item.title });
-          }
-        });
-      });
-    }
+  // 在当前标签页打开新的URL，并等待标签页加载完成
+  chrome.tabs.update({ url: url }, function (tab) {
+    // 在标签页加载完成后，向content.js发送消息
+    chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+      if (tabId === tab.id && changeInfo.status === "complete") {
+        chrome.tabs.onUpdated.removeListener(listener);
+        chrome.tabs.sendMessage(tabId, { command: 'show_title', title: item.title });
+      }
+    });
   });
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.command === 'start') {
-    count = request.count;
-    getRandomWeb();
-  }
+chrome.browserAction.onClicked.addListener((tab) => {
+  getRandomWeb();
 });
