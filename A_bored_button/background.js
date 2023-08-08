04aa6efee8dbe9d5313ffe1360dcb7f66d5210fa
Fujizx <fujizx@163.com>
2023-08-08 14:16:04 +0800

    remove unused permissions

--- A_bored_button/background.js
+++ A_bored_button/background.js
@@ -23,15 +23,21 @@ async function getRandomWeb() {
   const item = getRandomItem(data);
   const url = item.url;
 
-  // 在当前窗口打开新标签页，并等待标签页加载完成
-  chrome.tabs.create({ url: url, active: false }, function (tab) {
-    // 在标签页加载完成后，向content.js发送消息
-    chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-      if (tabId === tab.id && changeInfo.status === "complete") {
-        chrome.tabs.onUpdated.removeListener(listener);
-        chrome.tabs.sendMessage(tabId, { command: 'show_title', title: item.title });
-      }
-    });
+  // 获取当前活动的标签页
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currTab = tabs[0];
+    if (currTab) { // 如果存在一个活动的标签页
+      // 在当前标签页打开新的URL，并等待标签页加载完成
+      chrome.tabs.update(currTab.id, { url: url }, function (tab) {
+        // 在标签页加载完成后，向content.js发送消息
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+          if (tabId === tab.id && changeInfo.status === "complete") {
+            chrome.tabs.onUpdated.removeListener(listener);
+            chrome.tabs.sendMessage(tabId, { command: 'show_title', title: item.title });
+          }
+        });
+      });
+    }
   });
 }
 
